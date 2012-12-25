7f0028108bda34738da42ecfd930df154a1d0b59
Kornelije Sajler <xajler@gmail.com>
2012-12-25 18:45:28 +0100

    Initial commit. Created IntelliJ proj, created all LoginActivity widgets.

---
+++ src/com/metaintellect/eRacun/MainActivity.java
@@ -0,0 +1,10 @@
+package com.metaintellect.eRacun;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
